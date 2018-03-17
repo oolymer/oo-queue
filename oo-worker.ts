74cd060f3561a6d3d65c35c63f5f09d6145d6bd9
Benjamin Gudehus <hastebrot@gmail.com>
2018-03-17 12:48:32 +0100

    (feature) Create oo.Worker with d3-queue and tinyqueue.

---
+++ oo-worker.ts
@@ -0,0 +1,38 @@
+import { queue, Queue } from "d3-queue"
+import TinyQueue from "tinyqueue"
+
+export { queue, TinyQueue }
+
+export function worker() {
+  const t = new TinyQueue()
+  const q = queue()
+  return [t, q]
+}
+
+export class Worker {
+
+  tasks: TinyQueue
+  queue: Queue
+
+  constructor() {
+    this.tasks = new TinyQueue()
+    this.queue = queue()
+
+    this.tasks.push("foo")
+    this.tasks.push("bar")
+    this.tasks.push("baz")
+
+    for (const task of this.tasks.data) {
+      this.queue.defer(done => {
+        console.log(task)
+        done(null)
+      })
+    }
+
+    // this.queue.abort()
+    this.queue.awaitAll(error => {
+      console.log(error)
+    })
+  }
+
+}
